fedf714d40429d884c1754df19b553c672cf847b
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-10-17 13:35:05 +0330

    BinarySeach with Duplicate and MajorityElement haave been added to Week4 Course1

---
+++ Course1/Week4/BinarySearch2/BinarySearch2/BinarySearch2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch2
+{
+    internal class Program
+    {
+        static int binarysearch(int[] SortedArr, int key)
+        {
+            int left = 0;
+            int right = SortedArr.Length - 1;
+            while (left <= right)
+            {
+                int Mid = (left + right) / 2;
+                if (SortedArr[Mid] == key && Mid!=0&& SortedArr[Mid - 1] != key)
+                    return Mid;
+                else if (SortedArr[Mid] == key && Mid!=0&& SortedArr[Mid - 1] == key )
+                    right = Mid - 1;
+                else if (SortedArr[Mid] < key)
+                    left = Mid + 1;
+                else
+                    right = Mid - 1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int ArrayCounter = int.Parse(Console.ReadLine());
+            string[] SortedArray = Console.ReadLine().Split();
+            int[] MySortedInt = new int[ArrayCounter];
+            MySortedInt = Array.ConvertAll<string, int>(SortedArray, int.Parse);
+            int KeyCounter = int.Parse(Console.ReadLine());
+
+            string[] Keys = Console.ReadLine().Split();
+            int[] KeyArray = new int[ArrayCounter];
+            KeyArray = Array.ConvertAll<string, int>(Keys, int.Parse);
+            List<int> Result = new List<int>();
+            for (int i = 0; i < Keys.Length; i++)
+            {
+                if (KeyArray[i] == MySortedInt[0])
+                    Result.Add(0);
+                else
+                    Result.Add(binarysearch(MySortedInt, KeyArray[i]));
+            }
+                
+            Console.WriteLine(string.Join(" ", Result));
+        }
+
+    }
+}
---
+++ Course1/Week4/MajorityElement/MajorityElement/MajorityElement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ElementCounter = int.Parse(Console.ReadLine());
+            string[] InputDigits = Console.ReadLine().Split();
+            List<int> Elements = InputDigits.ToList().ConvertAll(int.Parse);
+            Elements.Sort();
+            Elements.Add(0);
+            int count = 1;
+            List<int> NumberCounter = new List<int>();
+            if (Elements.Count == 1)
+                Console.WriteLine(1);
+            else if (Elements.Count == 2)
+            {
+                if (Elements[0] == Elements[1])
+                    Console.WriteLine(1);
+                else
+                    Console.WriteLine(0);
+            }
+            else
+            {
+                for (int i = 0; i < ElementCounter; i++)
+                {
+                    if (Elements[i] == Elements[i + 1])
+                        count += 1;
+                    else
+                    {
+                        NumberCounter.Add(count);
+                        count = 1;
+                    }
+                }
+
+                int Indexer = 0;
+                foreach (var item in NumberCounter)
+                {
+                    if (item > (ElementCounter / 2))
+                        Indexer += 1;
+                }
+                Console.WriteLine(Indexer);
+            }
+        }
+    }
+}
